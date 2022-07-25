20b8d88d70f52e57d79a6fbaf6c87a209a957433
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-07-25 14:23:22 -0500

    add language and 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'shrimp';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Grimer', height: 0.9, types: 'poison'},
+  {name: 'Houndoom', height: 1.4, types: ['dark', 'fire']},
+  {name: 'Combee', height: 0.3, types: ['bug', 'flying']}
+];
