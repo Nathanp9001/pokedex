0e7894342eed864188ef005aa65c47d886a7e2e7
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-07-30 18:42:15 -0500

    display pokemon and their height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
   {name: 'Grimer', height: 0.9, types: 'poison'},
   {name: 'Houndoom', height: 1.4, types: ['dark', 'fire']},
-  {name: 'Combee', height: 0.3, types: ['bug', 'flying']}
+  {name: 'Combee', height: 0.3, types: ['bug', 'flying']},
+  {name: 'Groudon', height: 3.5, types: 'ground'}
 ];
+
+// display pokemon names
+for (let i=0; i < pokemonList.length; i++) {
+  // if pokemon height is at least 2, adds text "Wow, that's big!"
+ if (pokemonList[i].height >= 2) {
+  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') Wow, that\'s big!')
+ } else {document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
+ }
+}
